4a4599d7287e04c1c23fbf1ce2ee0e6658b4ecce
hendra.dotjang <hendra.dotjang@primecash.co.id>
2021-06-12 20:03:14 +0700

    Adding Certificate and Custom Handler and Adding new Feture for API

--- src/main/java/com/test/testForJavaBackend1/controller/FlickrController.java
+++ src/main/java/com/test/testForJavaBackend1/controller/FlickrController.java
@@ -3,10 +3,6 @@ package com.test.testForJavaBackend1.controller;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.scribejava.apis.FlickrApi;
 import com.github.scribejava.apis.FlickrApi.FlickrPerm;
 import com.github.scribejava.core.builder.ServiceBuilder;
-import com.github.scribejava.core.model.OAuth1AccessToken;
 import com.github.scribejava.core.model.OAuth1RequestToken;
 import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.model.Response;
@@ -28,9 +23,9 @@ import com.test.testForJavaBackend1.handler.ConstantsGlobal;
 public class FlickrController {
 
 	final String urls = "https://www.flickr.com/services/oauth/request_token";
-	private static final String PROTECTED_RESOURCE_URL = "https://www.flickr.com/services/rest/";
-	private static final String userId="193198752@N05";
+	protected static final String PROTECTED_RESOURCE_URL = "https://www.flickr.com/services/rest/";
 	private OAuth1RequestToken token = new OAuth1RequestToken(urls, PROTECTED_RESOURCE_URL);
+	OAuthRequest requestToken = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL); 
 
 	public OAuth1RequestToken getToken() {
 		return token;
@@ -39,51 +34,31 @@ public class FlickrController {
 	public void setToken(OAuth1RequestToken token) {
 		this.token = token;
 	}
-	@RequestMapping(value="/search", method=RequestMethod.GET)
+	@RequestMapping(value="/searchPhoto", method=RequestMethod.GET)
 	public @ResponseBody String searchFlickr(@RequestParam("search") String search, @RequestParam("page") String page) throws InterruptedException, ExecutionException, IOException {
 		OAuth10aService serv = new ServiceBuilder(ConstantsGlobal.API_KEY).apiSecret(ConstantsGlobal.API_SECRET).build(FlickrApi.instance(FlickrPerm.READ));
-		final OAuthRequest requestOauth = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
-		requestOauth.addQuerystringParameter("method", "flickr.photos.search");
-		requestOauth.addQuerystringParameter("tags", search);
-		requestOauth.addQuerystringParameter("format", "json");
-		requestOauth.addQuerystringParameter("page", page);
-		requestOauth.addQuerystringParameter("per_page", "10");
-		requestOauth.addQuerystringParameter("format", "json");
-		requestOauth.addQuerystringParameter("api_key", ConstantsGlobal.API_KEY);
-		Response rest = serv.execute(requestOauth);
-		return rest.getBody();
+		requestToken.addQuerystringParameter("method", "flickr.photos.search");
+		requestToken.addQuerystringParameter("tags", search);
+		requestToken.addQuerystringParameter("format", "json");
+		requestToken.addQuerystringParameter("page", page);
+		requestToken.addQuerystringParameter("per_page", "10");
+		requestToken.addQuerystringParameter("format", "json");
+		requestToken.addQuerystringParameter("api_key", ConstantsGlobal.API_KEY);
+		return serv.execute(requestToken).getBody();
 	}
-
-	@GetMapping("/")
-	public void home(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, InterruptedException, ExecutionException {
-
-		OAuth10aService service = new ServiceBuilder(ConstantsGlobal.API_KEY).apiSecret(ConstantsGlobal.API_SECRET)
-				.callback("http://localhost:9002/interview/call").build(FlickrApi.instance(FlickrApi.FlickrPerm.READ));
-		
-		//OAuthRequest resourceRequest = new OAuthRequest(Verb.GET, urls);
-		// resourceRequest = service.getRequestToken();
-		setToken(service.getRequestToken());
-		String urlsAuth = service.getAuthorizationUrl(getToken());
-		response.sendRedirect(urlsAuth + "&perms=read");
+	
+	@RequestMapping(value="list-comments", method=RequestMethod.GET)
+	public @ResponseBody String getCommentsByPhotoId(@RequestParam("photoId") String photoId) throws IOException, InterruptedException, ExecutionException {
+		OAuth10aService serv = new ServiceBuilder(ConstantsGlobal.API_KEY).apiSecret(ConstantsGlobal.API_SECRET).build(FlickrApi.instance(FlickrPerm.READ));
+		requestToken.addQuerystringParameter("method", "flickr.photos.search");
+		requestToken.addQuerystringParameter("photo_id", photoId);
+		requestToken.addQuerystringParameter("format", "json");
+		requestToken.addQuerystringParameter("api_key", ConstantsGlobal.API_KEY);
+		return serv.execute(requestToken).getBody();
 	}
+	
+	
 
-	@RequestMapping(value = "/call", method = RequestMethod.GET)
-	public String requestCall(@RequestParam("oauth_token") String requestParam,
-			@RequestParam("oauth_verifier") String oauthVerifier)
-			throws IOException, InterruptedException, ExecutionException {
-		OAuth10aService service = new ServiceBuilder(ConstantsGlobal.API_KEY).apiSecret(ConstantsGlobal.API_SECRET)
-				.callback("http://localhost:9002/interview/call").build(FlickrApi.instance(FlickrApi.FlickrPerm.READ));
-		final OAuth1AccessToken accessToken = service.getAccessToken(getToken(), oauthVerifier);
-		final OAuthRequest requestOauth = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
-		requestOauth.addQuerystringParameter("method", "flickr.people.getPublicPhotos");
-		requestOauth.addQuerystringParameter("user_id", userId);
-		requestOauth.addQuerystringParameter("format", "json");
-		service.signRequest(accessToken, requestOauth);
-		Response rest = service.execute(requestOauth);
-		System.out.println(rest.getBody());
-		return rest.getBody();
-
-	}
+	
 
 }
--- src/main/java/com/test/testForJavaBackend1/handler/SimpleCORSFilter.java
+++ src/main/java/com/test/testForJavaBackend1/handler/SimpleCORSFilter.java
@@ -1,23 +1,22 @@
 package com.test.testForJavaBackend1.handler;
-//package co.l.nostra.handler;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-//
-//@Configuration
-//public class SimpleCORSFilter {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
-//}
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SimpleCORSFilter {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
+}
