a0276d1ea653f9086aebde97a17ed59685041687
hendra.dotjang <hendra.dotjang@primecash.co.id>
2021-06-12 18:37:07 +0700

    adding new Class

---
+++ src/main/java/com/test/testForJavaBackend1/handler/ConstantsGlobal.java
@@ -0,0 +1,12 @@
+package com.test.testForJavaBackend1.handler;
+
+public class ConstantsGlobal {
+	public static final String OK = "1";
+	public static final String ERROR = "0";
+	public static final String MESSAGE_SUCCESS = "Ok";
+	public static final String MESSAGE_ERROR = "Error";
+	public static final Integer MAX_RELEASE = 10;
+	public static final String API_KEY = "3a5f4cfd310559bb62ad74024f732e59";
+	public static final String API_SECRET ="5fafaa8b04e92796";
+
+}
