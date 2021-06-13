2724c5abc18b04585272b45cc77c7eea5f44bf5c
hendra.dotjang <docang1505@gmail.com>
2021-06-13 13:47:41 +0700

    Adding Swagger ui with docker

--- src/main/java/com/test/testForJavaBackend1/SwaggerConfig.java
+++ src/main/java/com/test/testForJavaBackend1/SwaggerConfig.java
@@ -1,16 +1,34 @@
 package com.test.testForJavaBackend1;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
+@Configuration
 public class SwaggerConfig {
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfo("Spring Boot REST API", "Spring Boot REST API for User management", "1.0",
+				"Terms of service",
+				new Contact("Hello", "https://testforjavabeckendapplication.herokuapp.com/", "docang1505@gmail.com"),
+				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
 	}
 }
